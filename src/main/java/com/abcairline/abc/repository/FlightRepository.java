4d9decd2c9a7fe2ad8ceebf76253c1d2cd8a6600
eunju <eunju7757@gmail.com>
2023-11-28 15:08:10 +0900

    add FlightRepository

--- src/main/java/com/abcairline/abc/repository/FlightRepository.java
+++ src/main/java/com/abcairline/abc/repository/FlightRepository.java
@@ -1,2 +1,28 @@
-package com.abcairline.abc.repository;public class FlightRepository {
+package com.abcairline.abc.repository;
+
+import com.abcairline.abc.domain.Flight;
+import com.abcairline.abc.domain.User;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class FlightRepository {
+    private final EntityManager em;
+
+    public void save(Flight flight) {
+        em.persist(flight);
+    }
+
+    public Flight findOne(Long id) {
+        return em.find(Flight.class, id);
+    }
+
+    public List<Flight> findAll() {
+        return em.createQuery("select f from Flight f", Flight.class).getResultList();
+    }
+
 }
