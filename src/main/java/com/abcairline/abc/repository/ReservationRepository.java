ae171d359cb789e2de99c853cf4ec1b09440dec1
eunju <eunju7757@gmail.com>
2023-11-28 10:48:41 +0900

    add user and reservation repositories

---
+++ src/main/java/com/abcairline/abc/repository/ReservationRepository.java
@@ -0,0 +1,27 @@
+package com.abcairline.abc.repository;
+
+import com.abcairline.abc.domain.Reservation;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ReservationRepository {
+
+    private final EntityManager em;
+
+    public void save(Reservation reservation) {
+        em.persist(reservation);
+    }
+
+    public Reservation findOne(Long id) {
+        return em.find(Reservation.class, id);
+    }
+
+    public List<Reservation> findAll() {
+        return em.createQuery("select r from Reservation r", Reservation.class).getResultList();
+    }
+}
---
+++ src/main/java/com/abcairline/abc/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.abcairline.abc.repository;
+
+import com.abcairline.abc.domain.User;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+    private final EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findOne(Long id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+}
